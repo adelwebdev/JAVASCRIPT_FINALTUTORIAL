ab1f8e8d8ea6618551e8f03e155ed82d72eaa2d7
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-06-10 19:31:57 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+let chaine = "des lettres oui encore une fois";
+let number = 34;
+let boolean = true;
+console.log(chaine, number, boolean);
+
+let array = ["je", "suis", 24, false];
+console.log(array);
+
+let objet = {
+  prénom: "audrey",
+  age: 34,
+  ville: "Bordeaux",
+};
+
+console.log(objet);
+console.log(typeof number);
+
+let arbre = null;
+console.log(typeof arbre);
+
+let total = 2;
+let x = 4;
+
+total = ++x;
+console.log(total);
