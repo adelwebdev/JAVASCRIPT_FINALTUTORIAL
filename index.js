4541b443099ea4c60b8d7232b4c0cf72f27898bb
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-06-10 20:03:46 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -23,3 +23,27 @@ let x = 4;
 
 total = ++x;
 console.log(total);
+
+let xx = 2;
+let yy = "2";
+
+if (!xx) {
+  console.log("x n'existe pas");
+} else {
+  console.log("x existe: égale à " + xx);
+}
+if (xx === yy && x > 3) {
+  console.log("yes");
+} else {
+  console.log("false");
+}
+
+a = 3;
+b = "3";
+
+a === b ? console.log("vrai") : console.log("false!");
+
+function fct(x) {
+  console.log("ert" + x);
+}
+fct(23);
