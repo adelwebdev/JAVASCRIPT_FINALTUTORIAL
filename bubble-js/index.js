ee9ab5ec258cb2d49631eee5e0765e4c3c67aa92
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-06-12 22:58:11 +0300

    Add files via upload

---
+++ bubble-js/index.js
@@ -0,0 +1,34 @@
+const counterDisplay = document.querySelector("h3");
+let counter = 0;
+
+const bubbleMaker = () => {
+  const bubble = document.createElement("span");
+  bubble.classList.add("bubble");
+  document.body.appendChild(bubble);
+
+  console.log(Math.random() * 200 + 100);
+  const size = Math.random() * 200 + 100 + "px";
+  console.log(size);
+
+  bubble.style.height = size;
+  bubble.style.width = size;
+
+  bubble.style.top = Math.random() * 100 + 50 + "%";
+  bubble.style.left = Math.random() * 100 + "%";
+
+  const plusMinus = Math.random() > 0.5 ? 1 : -1;
+  bubble.style.setProperty("--left", Math.random() * 100 * plusMinus + "%");
+
+  bubble.addEventListener("click", () => {
+    counter++;
+    console.log(counter);
+    counterDisplay.textContent = counter;
+    bubble.remove();
+  });
+
+  setTimeout(() => {
+    bubble.remove();
+  }, 8000);
+};
+
+setInterval(bubbleMaker, 1000);
