f1975e1da7b88305903bcaf21e767c1b696a77cc
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-06-12 21:47:25 +0300

    Add files via upload

--- cours/script.js
+++ cours/script.js
@@ -27,13 +27,13 @@ questionContainer.addEventListener("click", () => {
 });
 
 btn1.addEventListener("click", () => {
-//   console.log("click");
+  //   console.log("click");
   response.classList.toggle("show-response"); //AJOUTER CLASSE
   //   response.style.visibility = "visible";
   response.style.background = "green"; //AJOUTER STYLE
 });
 btn2.addEventListener("click", () => {
-//   console.log("click");
+  //   console.log("click");
   response.classList.toggle("show-response");
   //   response.style.visibility = "visible";
   response.style.background = "red";
@@ -51,27 +51,27 @@ window.addEventListener("mousemove", (e) => {
 });
 
 window.addEventListener("mousedown", () => {
-//   console.log("test!");
+  //   console.log("test!");
   mousemove.style.transform = "scale(2) translate(-25%, -25%)";
 });
 window.addEventListener("mouseup", () => {
-//   console.log("test!");
+  //   console.log("test!");
   mousemove.style.transform = "scale(1) translate(-50%, -50%)";
   mousemove.style.border = "2px solid red";
 });
 
 questionContainer.addEventListener("mouseenter", () => {
-//   console.log("This is mouseEnter!");
+  //   console.log("This is mouseEnter!");
   questionContainer.style.background = "salmon";
   questionContainer.style.color = "black";
 });
 questionContainer.addEventListener("mouseout", () => {
-//   console.log("This is mouseOut!");
+  //   console.log("This is mouseOut!");
   questionContainer.style.background = "black";
   questionContainer.style.color = "whitesmoke";
 });
 response.addEventListener("mouseover", () => {
-//   console.log("and this, mousOver");
+  //   console.log("and this, mousOver");
   response.style.transform = "rotate(2deg)";
 });
 
@@ -92,7 +92,7 @@ const ring = (key) => {
 };
 
 document.addEventListener("keypress", (e) => {
-//   console.log("vous appuyer sur la touche " + e.key);
+  //   console.log("vous appuyer sur la touche " + e.key);
   key.textContent = 'La touche est "' + e.key + '"';
   if (e.key === "a") {
     keypressContainer.style.background = "brown";
@@ -110,7 +110,7 @@ const nav = document.querySelector("nav");
 // console.log(nav);
 
 window.addEventListener("scroll", () => {
-//   console.log(window.scrollY);
+  //   console.log(window.scrollY);
 
   if (window.scrollY > 120) {
     nav.style.top = 0;
@@ -131,21 +131,21 @@ let pseudo = "";
 let language = ";";
 
 inputName.addEventListener("input", (e) => {
-//   console.log("event on input!", e.target.value);
+  //   console.log("event on input!", e.target.value);
   pseudo = e.target.value;
-//   console.log(pseudo);
+  //   console.log(pseudo);
 });
 
 select.addEventListener("input", (e) => {
-//   console.log(e.target.value);
+  //   console.log(e.target.value);
   language = e.target.value;
-//   console.log(language);
+  //   console.log(language);
 });
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-//   console.log("Submitted!!");
-//   console.log(cgv.checked);
+  //   console.log("Submitted!!");
+  //   console.log(cgv.checked);
 
   if (cgv.checked) {
     document.querySelector("form > div").innerHTML = `
@@ -159,7 +159,7 @@ form.addEventListener("submit", (e) => {
 
 // **********************LOAD EVENTS*************************//
 window.addEventListener("load", () => {
-//   console.log("charger!");
+  //   console.log("charger!");
 });
 
 // ***********************FOR EACH***************************//
@@ -173,3 +173,138 @@ boxes.forEach((box) => {
   });
 });
 
+//******************* addEventLister Vs onClick ***************//
+
+// document.body.onclick = () => {
+//   console.log("je click!");
+// };
+
+// document.body.onclick = () => {
+//   console.log("je click 2 !");
+// };
+
+// document.body.onclick = () => {
+//   console.log("le scroll!");
+// };
+
+//Budling => fin (de base l'eventlister es tparametré en bubling)*********
+document.body.addEventListener(
+  "click",
+  () => {
+    console.log("click I");
+  },
+  false
+);
+
+//Use capture**********************************************
+document.body.addEventListener(
+  "click",
+  () => {
+    console.log("click II");
+  },
+  true
+);
+
+//---------------------------------
+// stop propagation--------------------------
+questionContainer.addEventListener("click", (e) => {
+  alert("attention");
+  e.stopPropagation();
+});
+
+//removeEventLister--------
+//--------------------------------------------
+
+//------------- LE BOM ----------------------//
+
+console.log(window.innerHeight);
+console.log(window.scrollY);
+// window.open("http://www.google.com", "cours js", "height=600, width=800");
+// window.close(), A ECRIRE DANS LA CONSOLE DE LA POP-UP POUR FERMER LA POP-UP
+// window.alert("ooo");
+
+//Evenements adossées à Window
+// alert("ooo");
+
+//confirm--------- IMPORTANT --------
+btn2.addEventListener("click", () => {
+  confirm("voulez-vous vraiment vous tromper?");
+});
+
+//prompt --------IMPORTANT AUSSI
+// btn1.addEventListener("click", () => {
+//   prompt("Entrer votre nom!");
+// });
+
+btn1.addEventListener("click", () => {
+  let answer = prompt("Entrer votre nom!");
+  questionContainer.innerHTML += "<h3>Bravo " + answer;
+});
+
+//setTimeOut------timer, compte à rebours---------------------
+setTimeout(() => {
+  questionContainer.style.borderRadius = "30%";
+}, 2000);
+
+// setInterval(() => {
+//   document.body.innerHTML += `<div class=box><h2>Nouvelle boite!</h2></div>`;
+// }, 1000);
+
+// let interval = setInterval(() => {
+//   document.body.innerHTML += `<div class=box><h2>Nouvelle boite!</h2></div>`;
+// }, 1000);
+
+// document.body.addEventListener("click", (e) => {
+//   e.target.remove();
+//   clearInterval(interval);
+// });
+
+//location--------------------
+// console.log(location.href);
+// console.log(location.host);
+// console.log(location.pathname);
+// console.log(location.search);
+
+// location.replace("http://lequipe.fr");
+
+// window.onload = () => {
+//   location.href = "http://twitter.fr";
+// };
+
+// navigator
+// console.log(navigator.userAgent);
+
+// https://developer.mozilla.org/fr/docs/Web/API/Geolocation/getCurrentPosition
+
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+};
+function success(pos) {
+  var crd = pos.coords;
+
+  console.log("Votre position actuelle est :");
+  console.log(`Latitude : ${crd.latitude}`);
+  console.log(`Longitude : ${crd.longitude}`);
+  console.log(`La précision est de ${crd.accuracy} mètres.`);
+}
+
+function error(err) {
+  console.warn(`ERREUR (${err.code}): ${err.message}`);
+}
+
+navigator.geolocation.getCurrentPosition(success, error, options);
+
+//History-----------------------------------------
+console.log(window.history);
+// window.history.back();
+// history.go(-2);
+
+//------------------------------------------------
+//-------- SET PROPERTY -----------------------------
+window.addEventListener("mousemove", (e) => {
+  console.log(e);
+  nav.style.setProperty("--x", e.layerX + "px");
+  nav.style.setProperty("--y", e.layerY + "px");
+});
