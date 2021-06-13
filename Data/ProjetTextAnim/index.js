690187e873dfa1c55e8427f5f60a9d999cca6740
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-06-14 02:10:29 +0300

    Add files via upload

---
+++ Data/ProjetTextAnim/index.js
@@ -0,0 +1,40 @@
+const target = document.getElementById("target");
+let array = ["Développeur", "Photographe", "Créatif"];
+let wordIndex = 0;
+let letterIndex = 0;
+
+const createLetter = () => {
+  const letter = document.createElement("span");
+  target.appendChild(letter);
+
+  letter.textContent = array[wordIndex][letterIndex];
+  setTimeout(() => {
+    letter.remove();
+  }, 2000);
+};
+
+// setInterval(() => {
+//   letterIndex++;
+//   createLetter();
+// }, 200);
+
+const loop = () => {
+  setTimeout(() => {
+    if (wordIndex >= array.length) {
+      wordIndex = 0;
+      letterIndex = 0;
+      loop();
+    } else if (letterIndex < array[wordIndex].length) {
+      createLetter();
+      letterIndex++;
+      loop();
+    } else {
+      wordIndex++;
+      letterIndex = 0;
+      setTimeout(() => {
+        loop();
+      }, 2000);
+    }
+  }, 100);
+};
+loop();
