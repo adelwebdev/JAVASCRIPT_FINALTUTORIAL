a9710c64862678e014ca518d6e2b437bc20d3198
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-06-13 21:45:30 +0300

    Add files via upload

---
+++ Data/index.js
@@ -0,0 +1,11 @@
+//---------------------------------types data-------------------------------------
+let string = "chaine";
+let number = 23;
+let boolean = true;
+let vare; // type Undefined
+
+//tableaux
+
+let tab = ["me", "you", "them"];
+
+console.log(tab[0]);
