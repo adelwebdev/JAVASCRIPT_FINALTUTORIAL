38cb5212d61cb6b87eb9e1d5837ab7a582cdf526
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-06-15 16:34:17 +0300

    Add files via upload

--- Data/password-maker/index.js
+++ Data/password-maker/index.js
@@ -2,22 +2,34 @@ const dataLowercase = "azertyuiopqsdfghjklmwxcvbn";
 const dataUppercase = dataLowercase.toUpperCase();
 const dataNumbers = "0123456789";
 const dataSymbols = "$*%!?;,.:/^@{}[]()#~&|<>";
+const rangeValue = document.getElementById("password-lenght");
+const passwordOutput = document.getElementById("password-output");
+
+// console.log(rangeValue.value);
+console.log(passwordOutput);
 
 function generatePassword() {
-  console.log("je génére un mot de pass");
-
   let dataPass = [];
-  console.log(lowercase.checked);
+  let password = "";
 
-  if (lowercase.checked) dataPass.push(dataLowercase);
-  if (uppercase.checked) dataPass.push(dataUppercase);
-  if (numbers.checked) dataPass.push(dataNumbers);
-  if (symbols.checked) dataPass.push(dataSymbols);
+  if (lowercase.checked) dataPass.push(...dataLowercase);
+  if (uppercase.checked) dataPass.push(...dataUppercase);
+  if (numbers.checked) dataPass.push(...dataNumbers);
+  if (symbols.checked) dataPass.push(...dataSymbols);
 
-  console.log(dataPass);
+  if (dataPass.length === 0) {
+    alert("Veuiller séléctionner des critères");
+    return;
+  }
+  for (i = 0; i < rangeValue.value; i++) {
+    // console.log(dataPass[Math.floor(Math.random() * dataPass.length)]);
+    password += dataPass[Math.floor(Math.random() * dataPass.length)];
+    // console.log(dataPass);
+    // console.log(password);
+  }
+  passwordOutput.value = password;
 }
 // genratePassword();  ----- appelle de la fonction--------
-
 // generateButton.addEventListener("click", () => {
 // //   console.log("ca marche!");
 // });
