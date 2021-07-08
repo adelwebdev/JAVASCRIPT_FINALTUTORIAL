eccf472a84877b67ee60004509e099cdb851a992
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-07-08 21:27:56 +0300

    Add files via upload

--- POO/yoga-routine/index.js
+++ POO/yoga-routine/index.js
@@ -16,18 +16,88 @@ let exerciceArray = [
 
 class Exercice {}
 
-const utils = {};
+const utils = {
+  pageContent: function (title, content, btn) {
+    document.querySelector("h1").innerHTML = title;
+    main.innerHTML = content;
+    document.querySelector(".btn-container").innerHTML = btn;
+  },
+  handleEventMinutes: function () {
+    document.querySelectorAll("input[type='number']").forEach((input) => {
+      input.addEventListener("input", (e) => {
+        exerciceArray.map((exo) => {
+          console.log("test");
+          if (exo.pic == e.target.id) {
+            exo.min = parseInt(e.target.value);
+            console.log(exerciceArray);
+          }
+        });
+      });
+    });
+  },
+  handleEventArrow: function () {
+    document.querySelectorAll(".arrow").forEach((arrow) => {
+      arrow.addEventListener("click", (e) => {
+        let position = 0;
+        // console.log(e);
+        exerciceArray.map((exo) => {
+          if (exo.pic == e.target.dataset.pic && position !== 0) {
+            [exerciceArray[position], exerciceArray[position - 1]] = [
+              exerciceArray[position - 1],
+              exerciceArray[position],
+            ];
+            // console.log(exerciceArray);
+            page.lobby();
+          } else {
+            position++;
+            // console.log(position);
+          }
+        });
+      });
+    });
+  },
+};
 
 const page = {
   lobby: function () {
-    document.querySelector("h1").innerHTML =
-      "Paramétrage <i id='reboot' class='fas fa-undo'></i>";
-    main.innerHTML = "Exercices";
-    document.querySelector(".btn-container").innerHTML =
-      "<button id='start'>Commencer<i class='far fa-play-circle'></i></button>";
+    let mapArray = exerciceArray
+      .map(
+        (exo) =>
+          `
+        <li>
+        <div class="card-header">
+        <input type="number" id=${exo.pic} min="1" max="10" value=${exo.min}>
+        <span>min</span>
+        </div>
+        <img src="./img/${exo.pic}.png"/>
+        <i class="fas fa-arrow-alt-circle-left arrow" data-pic=${exo.pic}></i>
+        <i class="fas fa-times-circle deleteBtn" data-pic=${exo.pic}></i>
+        </li>
+        `
+      )
+      .join(" ");
+
+    utils.pageContent(
+      "Paramétrage <i id='reboot' class='fas fa-undo'></i>",
+      "<ul>" + mapArray + "</ul>",
+      "<button id='start'>Commencer<i class='far fa-play-circle'></i></button>"
+    );
+    utils.handleEventMinutes();
+    utils.handleEventArrow();
   },
-  routine: function () {},
-  finish: function () {},
+
+  routine: function () {
+    utils.pageContent("Routine", "Exercice avec chrono", null);
+  },
+
+  finish: function () {
+    utils.pageContent(
+      "C'est terminé !",
+      "<button id='start'>Recommencer</button>",
+      "<button id='reboot' class='btn-reboot'>Réintitialiser<i class='fas fa-times-circle></i></button>"
+    );
+  },
 };
 
 page.lobby();
+utils.handleEventMinutes();
